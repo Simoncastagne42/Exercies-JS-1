a7f2037edd48b5fe60ed5e91fff5915b27d2fd95
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-13 15:44:07 +0100

    2e exo

---
+++ script.js
@@ -0,0 +1,19 @@
+// exercie 1
+// let guytou = "guytou";
+// alert(guytou);
+
+// exercice 2
+
+// let LastName = "Doe";
+// let FirstName = "John";
+// let city = "New-York";
+// window.confirm(
+//   "Nom :  " +
+//     LastName +
+//     "\n" +
+//     "Prénom : " +
+//     FirstName +
+//     "\n" +
+//     "Ville : " +
+//     city
+// );
