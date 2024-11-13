91e7ba9a077ad5925289bc2ed55a20442f4ffd85
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-13 16:32:32 +0100

    5e exo

--- script.js
+++ script.js
@@ -25,8 +25,21 @@
 // };
 
 // exercice 4
-let reponse = prompt("Quel est ton nom ?", "<Entrez ici votre nom>");
-if (reponse != null) {
-        prompt("Quel est votre prénom ?");
-        prompt("Quelle est votre ville ?");
-     };
+// let reponse = prompt("Quel est ton nom ?", "<Entrez ici votre nom>");
+// if (reponse != null) {
+//         prompt("Quel est votre prénom ?");
+//         prompt("Quelle est votre ville ?");
+//      };
+
+// exercice 5 
+let reponse = prompt("Donnez moi un nombres à virgules"); 
+let reponse2 = prompt("Donnez moi un nombres à virgules");
+
+let changenombre1= parseInt(reponse);
+let changenombre2= parseInt(reponse2);
+
+let resultat1 = Math.trunc(changenombre1);
+let resultat2 = Math.trunc(changenombre2);
+
+window.alert(resultat1*resultat2);
+
