47963315221ecc3b0c2ba4dd69a1bbfd892df853
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-14 14:07:58 +0100

    8e exo

--- script.js
+++ script.js
@@ -54,3 +54,12 @@
 //   return resultat;
 // }
 // console.log(calcul(reponsePointure,reponseAnnée));
+
+// exercice 8
+
+// let age = prompt("Quel est votre age ?");
+// if (age >= 18) {
+//   alert("Vous êtes majeur");
+// } else {
+//   alert("Vous  êtes mineur");
+// }
