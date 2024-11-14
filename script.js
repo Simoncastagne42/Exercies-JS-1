1a9570582aa6a5bf5c732136d50700610d3cbd1f
Simoncastagne42 <simoncastagne42@gmail.com>
2024-11-14 14:00:40 +0100

    7e exo

--- script.js
+++ script.js
@@ -24,22 +24,33 @@
 //     alert("Bonjour " + reponse);
 // };
 
-// exercice 4
-// let reponse = prompt("Quel est ton nom ?", "<Entrez ici votre nom>");
-// if (reponse != null) {
-//         prompt("Quel est votre prénom ?");
-//         prompt("Quelle est votre ville ?");
-//      };
+// // exercice 4
+//  let LastName = prompt("Quel est votre nom ?");
+//  let FirstName = prompt("Quel est votre prénom ?");
+//  let city = prompt("Quel est votre ville ?");
+//  window.confirm(
+//    "Bonjour :  " + LastName + " " + FirstName + " " + "de " + city
+//  );
 
-// exercice 5 
-let reponse = prompt("Donnez moi un nombre à virgules"); 
-let reponse2 = prompt("Donnez moi un nombre à virgules");
+// exercice 5
+// let reponse = prompt("Donnez moi un nombre à virgule");
+// let reponse2 = prompt("Donnez moi un nombre à virgule");
 
-let changenombre1= parseInt(reponse);
-let changenombre2= parseInt(reponse2);
+// let changenombre1= parseInt(reponse);
+// let changenombre2= parseInt(reponse2);
 
-let resultat1 = Math.trunc(changenombre1);
+// let resultat1 = Math.trunc(changenombre1);
 
+// window.alert(resultat1*reponse2);
 
-window.alert(resultat1*reponse2);
+// exercice 6
 
+// exercice 7
+// let reponsePointure = parseInt(prompt("Quelle est votre pointure ?"));
+// let reponseAnnée = parseInt(prompt("Quelle est votre année de naissance ?"));
+
+// function calcul(reponsePointure, reponseAnnée) {
+//   let resultat = (reponsePointure * 2 + 5) * 50 - reponseAnnée + 1766;
+//   return resultat;
+// }
+// console.log(calcul(reponsePointure,reponseAnnée));
